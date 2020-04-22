dcec27537c6416557afec4a2729828b9b5f65304
Loup Menjoulet <lmenjoulet@gmail.com>
2020-04-22 18:00:14 +0200

    ajout de usermanager

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+//modules nodejs
 let fs = require('fs');
 const Discord = require('discord.js');
+
+//librairies propres au projet
 
 //clé d'acces au bot
 const TOKEN  = fs.readFileSync("./ressources/discord_token.txt").toString();
