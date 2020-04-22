177ec52a0e07d5db0141cef2941cb034c1b21a30
Loup Menjoulet <lmenjoulet@gmail.com>
2020-04-22 18:06:21 +0200

    bricolage

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-//modules nodejs
 let fs = require('fs');
 const Discord = require('discord.js');
-
-//librairies propres au projet
 
 //clé d'acces au bot
 const TOKEN  = fs.readFileSync("./ressources/discord_token.txt").toString();
